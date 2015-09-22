2e24f3e89f124632f81df0bb7c688ecd19463148
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-09-22 11:18:40 -0600

    Update with angular

--- js/main.js
+++ js/main.js
@@ -1,44 +1,81 @@
-/*
-- Rock > scissors
-- Scissors > paper
-- Paper > rock
-- There can be a draw
-*/
+var app = angular.module('myApp', []);
 
-function Game(player1, player2) {
-  this.player1 = player1;
-  this.player2 = player2;
-}
+app.controller('myController', function($scope) {
 
-function Player(name) {
-  this.name = name;
-}
-
-Player.prototype.picks = function(choice) {
-  this.choice = choice;
-};
-
-Game.prototype.winner = function() {
-  switch(true) {
-    case this.player1.choice === 'rock' && this.player2.choice === 'scissors':
-      return this.player1;
-    case this.player1.choice === 'rock' && this.player2.choice === 'paper':
-      return this.player2;
-    case this.player1.choice === 'paper' && this.player2.choice === 'rock':
-      return this.player1;
-    case this.player1.choice === 'paper' && this.player2.choice === 'scissors':
-      return this.player2;
-    case this.player1.choice === 'scissors' && this.player2.choice === 'paper':
-      return this.player1;
-    case this.player1.chice === 'scissors' && this.player2.choice === 'rock':
-      return this.player2;
-    }
-    if (this.player1.choice === this.player2.choice) {
-      return null;
-    }
+  $scope.init = function(){
+    $scope.players = {
+      player1: {
+        choice: "",
+        score: 0,
+        tie: false
+      },
+      player2: {
+        choice: "",
+        score: 0,
+        tie: false
+      }
+    };
   };
 
-  module.exports = {
-    Game: Game,
-    Player: Player
+  $scope.init();
+
+  $scope.computerChoice = function(){
+    $scope.options = ["rock", "paper", "scissors", "lizard", "spock"];
+    $scope.players.player2.choice =  $scope.options[Math.floor(Math.random() * $scope.options.length)];
   };
+
+  $scope.winConditions = function(){
+
+    $scope.players.player1.tie = false;
+    $scope.players.player2.tie = false;
+
+    if ($scope.players.player1.choice === $scope.players.player2.choice) {
+      $scope.players.player1.tie = true;
+      $scope.players.player2.tie = true;
+    }
+
+    switch(true) {
+
+      case $scope.players.player1.choice === 'rock' && $scope.players.player2.choice === 'scissors' || 'lizard':
+        $scope.players.player1.score++;
+        break;
+
+      case $scope.players.player1.choice === 'paper' && $scope.players.player2.choice === 'rock' || 'spock':
+        $scope.players.player1.score++;
+        break;
+
+      case $scope.players.player1.choice === 'scissors' && $scope.players.player2.choice === 'paper' || 'lizard':
+        $scope.players.player1.score++;
+        break;
+
+      case $scope.players.player1.choice === 'spock' && $scope.players.player2.choice === 'scissors' || 'rock':
+        $scope.players.player1.score++;
+        break;
+
+      case $scope.players.player1.choice === 'lizard' && $scope.players.player2.choice === 'spock' || 'paper':
+        $scope.players.player1.score++;
+        break;
+
+      //player 2 wins
+      case $scope.players.player1.choice === 'rock' || 'spock' && $scope.players.player2.choice === 'paper':
+        $scope.players.player2.score++;
+        break;
+
+      case $scope.players.player1.choice === 'paper' || 'lizard' && $scope.players.player2.choice === 'scissors':
+        $scope.players.player2.score++;
+        break;
+
+      case $scope.players.player1.choice === 'scissors' || 'lizard' && $scope.players.player2.choice === 'rock':
+        $scope.players.player2.score++;
+        break;
+
+      case $scope.players.player1.choice === 'scissors' || 'rock' && $scope.players.player2.choice === 'spock':
+        $scope.players.player2.score++;
+        break;
+
+      case $scope.players.player1.choice === 'spock' || 'paper' && $scope.players.player2.choice === 'lizard':
+        $scope.players.player2.score++;
+        break;
+    }
+  };
+});
